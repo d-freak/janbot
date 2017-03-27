0b3971ee99e412155f77eb45807fc9f766f72128
masasutzu <masasutzu@yurius>
2017-03-28 00:15:04 +0900

    <仕様変更>sで中国麻雀が始まるよう変更

--- src/main/java/wiz/project/janbot/MessageListener.java
+++ src/main/java/wiz/project/janbot/MessageListener.java
@@ -63,11 +63,11 @@ class MessageListener<T extends PircBotX> extends ListenerAdapter<T> {
                 IRCBOT.getInstance().println("(  ；∀；)");
                 IRCBOT.getInstance().disconnect();
             }
-            else if (message.equals("s") || message.equals("start")) {
-                GameMaster.getInstance().onStartSolo(playerName);
-            }
-            else if (message.equals("s chm") || message.equals("start chm") || message.equals("chm s") || message.equals("chm start")) {
+            else if (message.equals("s") || message.equals("s chm") || message.equals("chm s")) {
                 GameMaster.getInstance().onStartChmSolo(playerName);
+            }
+            else if (message.equals("s jpm") || message.equals("jpm s")) {
+                GameMaster.getInstance().onStartSolo(playerName);
             }
             else if (message.equals("e") || message.equals("end")) {
                 GameMaster.getInstance().onEnd();
@@ -144,11 +144,11 @@ class MessageListener<T extends PircBotX> extends ListenerAdapter<T> {
             else if (message.startsWith("w ")) {
                 GameMaster.getInstance().onWatch(message.substring(2));
             }
-            else if (message.equals("replay")) {
-                GameMaster.getInstance().onReplay(playerName);
-            }
-            else if (message.equals("replay chm") || message.equals("chm replay")) {
+            else if (message.equals("replay") || message.equals("replay chm") || message.equals("chm replay")) {
                 GameMaster.getInstance().onReplayChm(playerName);
+            }
+            else if (message.equals("replay jpm") || message.equals("jpm replay")) {
+                GameMaster.getInstance().onReplay(playerName);
             }
             else if (message.startsWith("replay ")) {
                 GameMaster.getInstance().onReplay(playerName, message.substring(7));
@@ -186,8 +186,8 @@ class MessageListener<T extends PircBotX> extends ListenerAdapter<T> {
                                   "sy [X] [開始値-終了値] [-c表示する役の最大数] [-p表示する役の最小点]：",
                                   "指定したプレイヤーの役のゲーム統計を表示   sr：ランキングを表示",
                                   "※ ss, syはXにa llと指定すると全員分を表示、その場合範囲指定は無効",
-                                  "s：開始   s chm：中国麻雀を開始   e：終了",
-                                  "replay：リプレイ   replay chm：中国麻雀でリプレイ",
+                                  "s, s chm：中国麻雀を開始   s jpm：日本麻雀を開始   e：終了",
+                                  "replay, replay chm：中国麻雀でリプレイ   replay jpm：日本麻雀でリプレイ",
                                   "i：状態   r：捨て牌   d X：指定牌(ex.9p)を切る (X指定無し：ツモ切り、鳴きキャンセル)",
                                   "chi X：指定牌(ex.3p)を先頭牌としてチー   pon：ポン   kan X：指定牌でカン",
                                   "ron, hu：ロン   tsumo, hu：ツモ和了   u, undo：取り消し   h：コマンド履歴表示",
@@ -200,6 +200,11 @@ class MessageListener<T extends PircBotX> extends ListenerAdapter<T> {
             else if (message.equals("chm help")) {
                 final List<String> messageList =
                     Arrays.asList("chm s：中国麻雀を開始   chm replay：中国麻雀でリプレイ");
+                IRCBOT.getInstance().println(messageList);
+            }
+            else if (message.equals("jpm help")) {
+                final List<String> messageList =
+                    Arrays.asList("jpm s：日本麻雀を開始   jpm replay：日本麻雀でリプレイ");
                 IRCBOT.getInstance().println(messageList);
             }
             else if (message.startsWith("ri-chi!") || message.startsWith("りち！") || message.startsWith("りぃち！") || message.startsWith("りーち！") || message.startsWith("リーチ！")) {
